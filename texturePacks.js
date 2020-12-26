a4063402530d35b6ea22b7a3245bb65b8cfb0931
Douglas Kelly <douglaspkelly@gmail.com>
2020-12-25 21:00:06 -0500

    .

--- texturePacks.js
+++ texturePacks.js
@@ -27,9 +27,26 @@ function htmlForPack(pack) {
     let h = '<div class=packDiv onclick=setActivePack("' + pack.id + '")>';
     h += '<h3 class=packName>' + pack.name + '</h3>'
         + '<img class=packImg src=' + pack.img + '><br>'
-        + '<span class=packDescription>' + pack.description + '><br>'
+        + '<span class=packDescription>' + pack.description + '<br>'
         + '</div>';
     return h;
+}
+
+function getPackById(id) {
+    for (let i = 0; i < packs.length; i++) {
+        if (packs[i].id == id) {
+            return packs[i];
+        }
+    }
+    return null;
+}
+
+function setActivePack(id) {
+    let pack = getPackById(id);
+    if (pack !== null) {
+        g('right').style.display = '';
+        g('downloadForm').action = pack.filename;
+    }
 }
 
 function g(id) {
